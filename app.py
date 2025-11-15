47cc9d3cc766f32a4c0aa7174c38d156cbe304e9
Kassim Hassan <kassim.hassan@go.minneapolis.edu>
2025-11-14 23:29:26 -0600

    final commit - Flask project

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template, request  # NOT the same as requests 
-
+from github_api import get_github_user  # import function to get github user info     ??
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  # Homepage route
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html') # returns html index we have been working on
+
+@app.route('/get_user')
+def get_user_info():
+    # get user info from github api, and display on new page
+    # form data is in request.args
+    print('form data is', request.args) # requests.args is a dictionary
+
+    # get info from github and display; create a template in templates.
+
+    #username = requests.args['username']  # get username from form data
+    username = request.args.get('username') #returns None if no user found
+    user_info, error_message = get_github_user(username)  # TODO call function to get github user info; dictionary
+
+    if error_message:
+        return render_template('error.html', error=error_message)  # TODO pass in error message to template
+    else:
+        
+        return render_template('github.html', user_info=user_info )  # TODO pass in user info we get from github
+
+
+
+
+
 
 
 if __name__ == '__main__':
--- github_api.py
+++ github_api.py
@@ -1,1 +1,62 @@
 # TODO make requests to github API
+# logic for making requests to GitHub API
+# details we make to githib will be here
+
+import requests
+import logging
+
+# get github user info
+def get_github_user(username):
+
+    # 404 - username not found
+    # other errors
+    # success
+
+
+    # return a tuple of (data, error)
+    # if things work, return ( data, None)
+    
+    # if thngs don't , there's and error, retun( none, error)
+
+
+    try:
+            response = requests.get(f'https://api.github.com/users/{username}')
+            
+
+            if response.status_code ==404:
+                return None, f'username{username} not found'
+            response.raise_for_status()  # Raise an error for bad responses
+    # make request to github api to get user info
+            json_response = response.json()  # get json response from response
+            user_info = extract_user_info(json_response)
+            return user_info
+    except Exception as e:
+            logging.exception(e)
+            return None, 'Error connecting to GitHub'
+
+
+            response = requests.get(f'https://api.github.com/users/{username}')
+        #todo error handling
+
+            #json_response = response.json()
+
+            
+           # print('Error fetching user info:', e)
+           # return None
+
+
+
+# extract relevant user info from json response 
+#              ??
+def extract_user_info(json_response):
+
+    return{
+        'login': json_response.get('login'),
+        'name': json_response.get('name'),
+        'avatar_url': json_response.get('avatar_url'),
+        'home_page': json_response.get('html_url'),
+        'repos': json_response.get('public_repos')
+
+
+
+    }
